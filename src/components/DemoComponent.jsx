a0036c2df748109e437bb9a3652a1d26aab55b34
Dinakaran G <dg@healthgrades.com>
2020-01-14 15:13:14 +0530

    Intial Setups
    Intial Setups

---
+++ src/components/DemoComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class MyDemo extends React.Component {
+    render() {
+        return (          
+            <div><h1>Hello World Component</h1> </div>
+        );
+    }
+}
